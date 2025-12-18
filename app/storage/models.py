1ceaff034803fcb3e3d70e0cd025d9cd5f417cf2
Alexander Muravyakin <thewoalex@gmail.com>
2025-12-18 15:57:29 +0500

    (+) Order database model

--- app/storage/models.py
+++ app/storage/models.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+from typing import List
 from uuid import UUID
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import JSON as pgJSON, ENUM as pgENUM
+
+from app.domain.models import OrderStatus
 
 
 class Base(DeclarativeBase):
@@ -17,3 +23,16 @@ class User(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column()
+    orders: Mapped[List['Order']] = relationship('Order', back_populates='user')
+
+
+class Order(Base):
+    """ Заказ """
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey(User.id))
+    user: Mapped['User'] = relationship('User', back_populates='orders')
+    items: Mapped[dict] = mapped_column(pgJSON)
+    total_price: Mapped[float] = mapped_column()
+    status: Mapped[OrderStatus] = mapped_column(pgENUM(OrderStatus, name='order_status'))
+    created_at: Mapped[datetime] = mapped_column()
