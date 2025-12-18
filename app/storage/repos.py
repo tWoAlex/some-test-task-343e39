d366945a1bf529b1e711032e346a735fbf6b872d
Alexander Muravyakin <thewoalex@gmail.com>
2025-12-18 15:58:22 +0500

    (+) OrderRepository

--- app/storage/repos.py
+++ app/storage/repos.py
@@ -1,10 +1,12 @@
+from uuid import UUID
+
 from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.domain.models import User as DomainUser
-from .models import User as DBUser
+from app.domain.models import Order as DomainOrder, User as DomainUser
+from .models import Order as DBOrder, User as DBUser
 
 
 class UserRepository:
@@ -45,3 +47,55 @@ class UserRepository:
         from_db = await self._session.scalar(query)
         if from_db:
             return self._db_to_domain_model(from_db)
+
+
+class OrderRepository:
+    """ Репозиторий заказов """
+
+    def __init__(self, session: AsyncSession):
+        self._session = session
+
+    def _db_to_domain_model(self, from_db: DBOrder):
+        """ Превратить модель из БД в доменную """
+
+        return DomainOrder(
+            id=from_db.id,
+            user_id=from_db.user_id,
+            items=from_db.items,
+            total_price=from_db.total_price,
+            status=from_db.status,
+            created_at=from_db.created_at
+        )
+
+    async def put(self, order: DomainOrder) -> DomainOrder:
+        """ Добавить/обновить заказ """
+
+        data = order.model_dump()
+        id = data.pop('id')
+
+        query = (
+            insert(DBOrder)
+            .values(id=id, **data)
+            .on_conflict_do_update(constraint=DBOrder.__table__.primary_key, set_=data)
+            .returning(DBOrder)
+        )
+        async with self._session.begin():
+            from_db = await self._session.scalar(query)
+        return self._db_to_domain_model(from_db)
+
+    async def get(self, order_id: UUID) -> DomainOrder | None:
+        """ Заказ по ID """
+
+        query = select(DBOrder).where(DBOrder.id == order_id)
+        async with self._session.begin():
+            from_db = await self._session.scalar(query)
+        if from_db is not None:
+            return self._db_to_domain_model(from_db)
+
+    async def get_for_user(self, user_id: UUID) -> list[DomainOrder]:
+        """ Заказы пользователя """
+
+        query = select(DBOrder).where(DBOrder.user_id == user_id)
+        async with self._session.begin():
+            from_db = await self._session.scalars(query)
+        return [self._db_to_domain_model(order) for order in from_db]
