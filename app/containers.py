da0ae3e357a1a758785a62cc41c8938939474678
Alexander Muravyakin <thewoalex@gmail.com>
2025-12-18 17:08:29 +0500

    (+) OrderRepository caching

--- app/containers.py
+++ app/containers.py
@@ -1,6 +1,8 @@
 from dependency_injector.containers import DeclarativeContainer, WiringConfiguration
 from dependency_injector.providers import Factory, Object, Singleton
 
+from redis.asyncio import Redis as AsyncRedis
+
 from sqlalchemy.engine import URL as DB_URL
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
@@ -32,9 +34,12 @@ class Container(DeclarativeContainer):
     db_engine = Singleton(create_async_engine, url=db_url)
     db_session = Factory(AsyncSession, bind=db_engine, expire_on_commit=False)
 
+    # Клиент Redis
+    redis_client = Singleton(AsyncRedis, host=config.REDIS_HOST, port=config.REDIS_PORT)
+
     # Репозитории
     user_repo = Factory(UserRepository, session=db_session)
-    order_repo = Factory(OrderRepository, session=db_session)
+    order_repo = Factory(OrderRepository, session=db_session, redis_client=redis_client)
 
     # Сервисы
     user_service = Factory(UserService, repo=user_repo)
--- app/storage/repos.py
+++ app/storage/repos.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timezone
 from uuid import UUID
+
+from redis.asyncio import Redis as AsyncRedis
 
 from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import insert
@@ -7,6 +10,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.domain.models import Order as DomainOrder, User as DomainUser
 from .models import Order as DBOrder, User as DBUser
+
+from app.config import config
 
 
 class UserRepository:
@@ -53,7 +58,11 @@ class UserRepository:
 class OrderRepository:
     """ Репозиторий заказов """
 
-    def __init__(self, session: AsyncSession):
+    CACHE_ORDER_LAST_REQUEST_KEY_TEMPLATE = 'order_request:<{order_id}>'
+    CACHE_ORDER_KEY_TEMPLATE = 'order:<{order_id}>'
+
+    def __init__(self, session: AsyncSession, redis_client: AsyncRedis):
+        self._redis = redis_client
         self._session = session
 
     def _db_to_domain_model(self, from_db: DBOrder):
@@ -67,6 +76,46 @@ class OrderRepository:
             status=from_db.status,
             created_at=from_db.created_at
         )
+
+    async def _get_from_cache(self, order_id: UUID) -> DomainOrder | None:
+        """ Забрать из кэша """
+
+        key = self.CACHE_ORDER_KEY_TEMPLATE.format(order_id=order_id)
+        from_cache = await self._redis.get(key)
+        if from_cache is not None:
+            return DomainOrder.model_validate_json(from_cache)
+
+    async def _cache_probed_earlier(self, order_id: UUID) -> bool:
+        """ Запрашивался ли ранее. Если нет, регистрирует текущий запрос """
+
+        last_request_key = self.CACHE_ORDER_LAST_REQUEST_KEY_TEMPLATE.format(order_id=order_id)
+
+        # Помечаем последний запрос заказа
+        if await self._redis.get(last_request_key) is None:
+            await self._redis.set(
+                last_request_key,
+                str(datetime.now(tz=timezone.utc).replace(tzinfo=None)),
+                ex=config.ORDER_CACHE_TTL
+            )
+            return False
+        return True
+
+    async def _put_to_cache(self, order: DomainOrder) -> None:
+        """ Добавить в кэш """
+
+        key = self.CACHE_ORDER_KEY_TEMPLATE.format(order_id=order.id)
+        await self._redis.set(
+            key,
+            order.model_dump_json(),
+            ex=config.ORDER_CACHE_TTL,
+            keepttl=True
+        )
+
+    async def _remove_from_cache(self, order_id: UUID) -> None:
+        """ Удалить из кэша """
+
+        key = self.CACHE_ORDER_KEY_TEMPLATE.format(order_id=order_id)
+        await self._redis.delete(key)
 
     async def put(self, order: DomainOrder) -> DomainOrder:
         """ Добавить/обновить заказ """
@@ -82,16 +131,26 @@ class OrderRepository:
         )
         async with self._session.begin():
             from_db = await self._session.scalar(query)
+        await self._remove_from_cache(id)
         return self._db_to_domain_model(from_db)
 
     async def get(self, order_id: UUID) -> DomainOrder | None:
         """ Заказ по ID """
 
+        from_cache = await self._get_from_cache(order_id)
+        if from_cache is not None:
+            return from_cache
+
         query = select(DBOrder).where(DBOrder.id == order_id)
         async with self._session.begin():
             from_db = await self._session.scalar(query)
-        if from_db is not None:
-            return self._db_to_domain_model(from_db)
+        if from_db is None:
+            return
+
+        order = self._db_to_domain_model(from_db)
+        if await self._cache_probed_earlier(order_id):
+            await self._put_to_cache(order)
+        return order
 
     async def get_for_user(self, user_id: UUID) -> list[DomainOrder]:
         """ Заказы пользователя """
