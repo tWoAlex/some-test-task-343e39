c01617032bab7d4f92ca479e0ed701b2d848314c
Alexander Muravyakin <thewoalex@gmail.com>
2025-12-18 15:57:09 +0500

    (+) Order domain model

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timezone
+from enum import StrEnum
+from typing import Any, Dict
 from uuid import UUID, uuid7
 
 from pydantic import BaseModel, Field
@@ -14,3 +17,32 @@ class User(BaseModel):
 
     hashed_password: str
     """ Хешированный пароль """
+
+
+class OrderStatus(StrEnum):
+    CANCELLED = 'CANCELLED'
+    PAID = 'PAID'
+    PENDING = 'PENDING'
+    SHIPPED = 'SHIPPED'
+
+
+class Order(BaseModel):
+    """ Заказ """
+
+    id: UUID = Field(default_factory=uuid7)
+    """ ID """
+
+    user_id: UUID
+    """ ID пользователя """
+
+    items: Dict[str, Any]
+    """ Список товаров """
+
+    total_price: float
+    """ Сумма """
+
+    status: OrderStatus = OrderStatus.PENDING
+    """ Статус заказа """
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc).replace(tzinfo=None))
+    """ Дата создания (по Гринвичу) """
